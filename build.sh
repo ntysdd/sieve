6fef2a3c9b938dd0c9310c511f2f8ee13342c5c8
ntysdd <ntysdd@gmail.com>
2025-01-15 21:48:51 +0800

    add count_solution.c

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/bash
 gcc -O2 -static -Wall -W -o sieve sieve.c
 gcc -O2 -static -Wall -W -o appro appro.c
+gcc -O2 -static -Wall -W -o count_solution count_solution.c
---
+++ count_solution.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+#define N 100000
+
+char p[N];
+
+int main()
+{
+    for (int i = 2; i < N; i++)
+    {
+        p[i] = 1;
+    }
+
+    for (int i = 2; i * i < N; i++)
+    {
+        for (int j = i * i; j < N; j += i)
+        {
+            p[j] = 0;
+        }
+    }
+
+    for (int s = 10; s <= N; s += 2)
+    {
+        int count = 0;
+        for (int i = 3; i <= s - 3; i += 2)
+        {
+            if (p[i] && p[s - i])
+            {
+                count++;
+            }
+        }
+        printf("%d %d\n", s, count);
+    }
+}
