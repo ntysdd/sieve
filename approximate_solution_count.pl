5d7ad2721403bb4d5370ba33a3090a90ae7d3f89
ntysdd <ntysdd@gmail.com>
2025-01-15 22:46:37 +0800

    reformat

--- approximate_solution_count.pl
+++ approximate_solution_count.pl
@@ -5,25 +5,19 @@ use strict;
 
 my $s = 1000000;
 
-
-for (my $i = 10; $i <= $s; $i += 2)
-{
+for ( my $i = 10 ; $i <= $s ; $i += 2 ) {
     my $r = f($i);
     print "$i $r\n";
 }
 
-sub is_prime
-{
+sub is_prime {
     my $x = shift;
-    if ($x <= 1)
-    {
+    if ( $x <= 1 ) {
         return 0;
     }
 
-    for (my $i = 2; $i * $i <= $x; $i++)
-    {
-        if ($x % $i == 0)
-        {
+    for ( my $i = 2 ; $i * $i <= $x ; $i++ ) {
+        if ( $x % $i == 0 ) {
             return 0;
         }
     }
@@ -31,17 +25,14 @@ sub is_prime
     return 1;
 }
 
-sub f
-{
-    my $n = shift;
-    my $sqrt = int(sqrt($n));
+sub f {
+    my $n    = shift;
+    my $sqrt = int( sqrt($n) );
 
     my $r = 0.5 * $n;
 
-    for (my $i = 3; $i <= $sqrt; $i++)
-    {
-        if (is_prime($i))
-        {
+    for ( my $i = 3 ; $i <= $sqrt ; $i++ ) {
+        if ( is_prime($i) ) {
             $r *= 1 - 2 / $i;
         }
     }
